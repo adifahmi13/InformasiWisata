3ee0661195136dd8ef908b1a1df7da559b8c4dc6
adifahmi13 <adifahmiaja@gmail.com>
2018-05-23 17:12:46 +0800

    final

---
+++ Apk_InformasiWisata/app/src/main/java/com/example/ryanadiwiguna/apk_informasiwisata/MainMenu.java
@@ -0,0 +1,22 @@
+package com.example.ryanadiwiguna.apk_informasiwisata;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void pindah(View view) {
+        Intent p = new Intent(MainMenu.this , MainActivity.class);
+        startActivity(p);
+    }
+
+
+}
